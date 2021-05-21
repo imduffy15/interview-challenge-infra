48486e19c02791283ef05cdade4a7929fcf326f8
Ian Duffy <ian@ianduffy.ie>
2021-05-21 11:36:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# Interview Challenge Infrastructure
+
+## Plan
+
+This repository will contain all of the infrastructure related components required to complete the spin interview challenge.
+
+This will include:
+
+AWS Infrastructure (terraform):
+
+ - VPC, subnets, security groups, etc.
+ - KMS key for encrypting secrets
+ - EKS Cluster, worker nodes, related IAM roles
+ - Container registry
+
+Software (helmfile):
+
+ - cert-manager: provide wildcard ssl certs for the team's application(s)
+ - external-dns: auto publishing of dns records for team's application(s)
+ - nginx-ingress: exposes http services
+ - kubed: syncing secrets across namespaces
+ - ci/cd: for deploying applications, will be provided by https://woodpecker.laszlo.cloud/
+ - logging-operator: enable logging of team's application(s) to cloudwatch logs
+ - prometheus/grafana: enable monitoring/metrics of team's application(s)
+ - shared secrets: secrets required for different components
+ - oauth2 proxy: provide SSO on top of grafana/prometheus/alert manager
+
+## Description
+
+It is assumed that this will be manually run by a devops engineer. Alternatively, it could be ran on some devops team owned infrastructure that is parameterised to quickly spin up new AWS accounts and infrastructure for teams.
+
+The following utilities are required:
+
+ - Kubectl
+ - Helm
+ - Helmfile
+ - SOPs
+ - Terraform
+ - AWSCLI
+
+---
+**NOTE**
+
+All secrets are encrypted with SOPs using an AWS KMS key, as a result, an AWS KMS key is required before creating any configuration. 
+
+The creation of this KMS key has been automated in ./prerequisites , In a real world scenario this KMS key would be
+owned by the devops team and re-used across devops owned projects.
+
+A seperate KMS key is generated for the development team of the notejam application.
+
+KMS key can be generated as follows:
+
+```
+$ cd prerequisites
+$ terraform init
+$ terraform apply
+```
+
+A sops configuration file will be outputted to ./generated_configs
+
+---
+
+---
+**NOTE**
+
+An S3 bucket is required for storing terraform state, this has been created manually with versioning enabled.
+
+---
+
+---
+**NOTE**
+
+
+The DNS zone for this is hosted outside of the provided AWS account. IAM access keys are provided to interact with the zone.
+
+---
+
+---
+**NOTE**
+
+
+Credentials for the CI to execute the deploy need to be created and imported into the CI system.
+This is done for two reasons:
+
+1) The CI system is doing docker in docker and injecting service account credentials is complicated. While doable, the hacks
+involved likely introduce more risk than static credentials.
+
+2) It is assumed that in a production environment the CI system would not run within the same cluster
+
+
+Relevant credentials/information can be acquired with:
+
+```
+$ kubectl get secret $(kubectl get secret | grep notejam-ci-user-token | awk '{print $1}') -o json | jq ".data | map_values(@base64d)"
+$ kubectl cluster-info
+```
+
+---
+
+
+## Services
+
+Accessible with GSuite SSO for `*@spin.pm` and `*@ianduffy.ie`
+
+ - https://grafana.chllng.ianduffy.ie
+ - https://alertmanager.chllng.ianduffy.ie
+ - https://prometheus.chllng.ianduffy.ie
+ - https://woodpecker.chllng.ianduffy.ie
---
+++ main.tf
@@ -0,0 +1,348 @@
+terraform {
+  backend "s3" {
+    bucket = "interview-challenge-terraform-states"
+    key    = "infrastructure"
+    region = "us-west-2"
+  }
+}
+
+locals {
+  prefix          = "interview-challenge"
+  aws_region      = "us-west-2"
+  db_notejam_name = "notejam"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+data "aws_availability_zones" "available" {
+}
+
+# NETWORKING
+
+# VPC
+# private networks unused, may be introduced time depending, not wanting to
+# deal with the complexities and costs (NAT Gateways) they introduce for now.
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 2.47"
+
+  name             = "${local.prefix}-vpc"
+  cidr             = "10.0.0.0/16"
+  azs              = data.aws_availability_zones.available.names
+  private_subnets  = []
+  database_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway = false
+
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.prefix}-eks" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
+
+# Default security group, everything has access to each other
+
+resource "aws_security_group" "default" {
+  name        = "${local.prefix}-default"
+  description = "Default security group for ${local.prefix}-eks"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = -1
+    self      = true
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# EKS
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "${local.prefix}-eks"
+  cluster_version = "1.17"
+  subnets         = module.vpc.public_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                 = "${local.prefix}-default-node-pool"
+      instance_type        = "t3.medium"
+      asg_desired_capacity = 3
+      bootstrap_extra_args = "--enable-docker-bridge true"
+    }
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.default.id]
+  write_kubeconfig                     = false
+  enable_irsa                          = true
+  cluster_log_retention_in_days        = 7
+  cluster_enabled_log_types            = ["api", "audit", "authenticator"]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+resource "local_file" "kubeconfig" {
+  filename = "${path.module}/generated_configs/kubeconfig"
+  content = templatefile("${path.module}/templates/kubeconfig.yaml.tpl", {
+    endpoint            = data.aws_eks_cluster.cluster.endpoint
+    cluster_auth_base64 = data.aws_eks_cluster.cluster.certificate_authority[0].data
+    cluster_arn         = data.aws_eks_cluster.cluster.arn
+    cluster_name        = data.aws_eks_cluster.cluster.id
+    region              = local.aws_region
+  })
+  file_permission = 0600
+}
+
+data "aws_iam_policy_document" "fluentd" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = format("%s:sub", replace(module.eks.cluster_oidc_issuer_url, "https://", ""))
+      values   = ["system:serviceaccount:kube-system:logging-infrastructure-fluentd"]
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "fluentd" {
+  name               = "${local.prefix}-fluentd"
+  assume_role_policy = data.aws_iam_policy_document.fluentd.json
+}
+
+data "aws_iam_policy_document" "fluentd-policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogGroup",
+      "logs:PutRetentionPolicy",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "fluentd" {
+  name   = "${local.prefix}-fluentd"
+  role   = aws_iam_role.fluentd.id
+  policy = data.aws_iam_policy_document.fluentd-policy.json
+}
+
+# Helm
+
+terraform {
+  required_providers {
+    helmfile = {
+      source = "mumoshu/helmfile"
+    }
+  }
+}
+
+provider "helmfile" {}
+
+resource "helmfile_release_set" "services" {
+  helm_binary       = "helm3"
+  working_directory = path.module
+  kubeconfig        = "${path.module}/generated_configs/kubeconfig"
+  depends_on = [
+    local_file.kubeconfig
+  ]
+}
+
+# DATABASE
+
+resource "random_string" "db_password" {
+  length  = 24
+  special = false
+}
+
+resource "aws_db_instance" "notejam_db" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7.33"
+  instance_class       = "db.t3.micro"
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+  identifier           = "${local.prefix}-rds"
+  skip_final_snapshot  = true
+
+  multi_az = true
+
+  backup_retention_period = 14
+
+  vpc_security_group_ids = [
+    aws_security_group.default.id
+  ]
+
+  name                = local.db_notejam_name
+  username            = local.db_notejam_name
+  password            = random_string.db_password.result
+}
+
+resource "local_file" "dbconfig" {
+  filename = "${path.module}/generated_configs/application.yaml"
+  content = templatefile("${path.module}/templates/spring-db-config.yaml.tpl", {
+    endpoint    = aws_db_instance.notejam_db.endpoint
+    db_name     = local.db_notejam_name
+    db_password = random_string.db_password.result
+  })
+  file_permission = 0600
+}
+
+# KMS Key
+
+resource "aws_kms_key" "notejam" {
+  description             = "${local.prefix} key for encrypting notejam secrets"
+  deletion_window_in_days = 7
+}
+
+resource "local_file" "sopsconfig" {
+  filename = "${path.module}/generated_configs/notejam-sops.yaml"
+  content = templatefile("${path.module}/templates/sops-config.yaml.tpl", {
+    kms_arn = aws_kms_key.notejam.arn
+  })
+  file_permission = 0600
+}
+
+resource "aws_ecr_repository" "registry" {
+  name = "${local.prefix}-notejam"
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+# CI
+
+resource "aws_iam_user" "ci_user" {
+  name = "${local.prefix}-notejam-ci-user"
+}
+
+resource "aws_iam_access_key" "ci_user" {
+  user = aws_iam_user.ci_user.name
+}
+
+resource "local_file" "ci_user" {
+  filename        = "${path.module}/generated_configs/ci-aws-creds.txt"
+  content         = "${aws_iam_access_key.ci_user.id}\n${aws_iam_access_key.ci_user.secret}\n"
+  file_permission = 0600
+}
+
+resource "aws_iam_user_policy" "ci_user" {
+  name = "${local.prefix}-notejam-ci-user"
+  user = aws_iam_user.ci_user.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:PutImage"
+            ],
+            "Resource": "${aws_ecr_repository.registry.arn}"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": "${aws_kms_key.notejam.arn}"
+        }
+    ]
+}
+EOF
+}
+
+# SMTP
+
+resource "aws_iam_user" "smtp_user" {
+  name = "${local.prefix}-notejam-smtp-user"
+}
+
+resource "aws_iam_access_key" "smtp_user" {
+  user = aws_iam_user.smtp_user.name
+}
+
+resource "local_file" "smtp_user" {
+  filename        = "${path.module}/generated_configs/smtp-creds.txt"
+  content         = "${aws_iam_access_key.smtp_user.id}\n${aws_iam_access_key.smtp_user.ses_smtp_password_v4}\n"
+  file_permission = 0600
+}
+
+resource "aws_iam_user_policy" "smtp-user" {
+  name = "${local.prefix}-notejam-smtp-user"
+  user = aws_iam_user.smtp_user.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ses:SendRawEmail"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
